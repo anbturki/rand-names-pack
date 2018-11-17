2ae6ac1ce02d971922ee0f320a2adbb295c2bb09
Barakat Turki <barakat.turki@gmail.com>
2018-11-17 19:42:22 +0200

    specify code coverage validation, add git hooks to packages.json

--- src/index.js
+++ src/index.js
@@ -1,14 +1,6 @@
-'use strict';
-
 var names = require("./names");
 var uniqueRandomArray = require("unique-random-array");
 module.exports = {
   all: names,
-  random: uniqueRandomArray(names),
-  printName: function (name) {
-    if (name === "ali") {
-      return "ali";
-    }
-    return name;
-  }
+  random: uniqueRandomArray(names)
 }
